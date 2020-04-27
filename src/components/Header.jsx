cf59bb095e0119c873d8396be040b66189e9e3f7
Alex <ajf@fastmail.com>
2020-04-27 11:20:17 -0500

    clase 22 register con redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { logoutRequest } from '../actions';
 import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
@@ -9,6 +10,9 @@ import userIcon from '../assets/static/user-icon.png';
 const Header = props => {
   const { user } = props;
   const hasUser = Object.keys(user).length > 0;
+  const handleLogout = () => {
+    props.logoutRequest({});
+  }
   return (
     <header className='header'>
       <Link to='/'>
@@ -22,13 +26,20 @@ const Header = props => {
           }
           <p>Perfil</p>
         </div>
+
         <ul>
-          <li><a href='/'>Cuenta</a></li>
-          <li>
-            <Link to='/login'>
-              Iniciar Sesión
-            </Link>
-          </li>
+          {hasUser ?
+            <li><a href='/'>{user.email}</a></li> :
+            null
+          }
+          {hasUser ?
+            <li><a href='#logout' onClick={handleLogout}>Cerrar sesión</a></li> :
+            <li>
+              <Link to='/login'>
+                Iniciar sesión
+              </Link>
+            </li>
+          }
         </ul>
       </div>
     </header>
@@ -41,4 +52,8 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
